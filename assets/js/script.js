dbb1a93d82e2e72a2dc8d18dfee3eca03c0a5bdb
Kelly Wacker <kelwacker@gmail.com>
2022-06-19 13:46:01 -0400

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,26 @@ var skills = {};
 $(".addBtn").on("click", function() {
     var skill = document.getElementById("skill").value;
     localStorage.setItem("Skills", skill);
-})
+});
+
+// dragon api
+var game = new Phaser.Game(800, 600, Phaser.WEBGL, 'phaser-example', { preload: preload, create: create });
+
+function preload() {
+    game.load.image('sky', 'assets/skies/deepblue.png');
+    game.load.image('dragonTexture', 'assets/creature/dragon.png');
+    game.load.json('dragonMesh', 'assets/creature/dragon.json');
+}
+
+var dragon = null;
+
+function create() {
+    game.add.image(0, 0, 'sky');
+
+    dragon = game.add.creature(450, 350, 'dragonTexture', 'dragonMesh');
+
+    dragon.scale.set(25.0);
+    
+    dragon.play(true); //  true = loop
+};
+
