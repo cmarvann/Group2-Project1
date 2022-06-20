43099c7b9b13e1fff5117cfcdcae053d243eeca9
crainone4037 <rainonecarmine0@gmail.com>
2022-06-20 13:08:12 -0400

    added a fetch function

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -2,3 +2,91 @@ var skill = "";
 var skillClicked = "";
 var skillClose = "";
 var reset = "";
+
+
+// container for skills div
+
+var skillEl = document.querySelector("#multiple-skills");
+
+//container for skills-list div 
+var addSkillel = document.querySelector("#add-skill");
+
+
+
+
+
+
+
+
+
+
+
+//function will fetch the skills and convert to JSON
+
+
+
+var getSkillsName = function(){
+
+
+
+
+
+    fetch(dndUrl).then(function (response) {
+
+        console.log(dndUrl);
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+
+            })
+        }
+
+        
+
+
+    });
+    getSkillsName();
+
+
+
+
+    
+
+} 
+
+
+
+
+
+//  document.querySelector(".single-skill").addEventListener("click",function(event){
+//      console.log("hello world");
+//      console.log(event.target);
+
+     //  })
+
+
+// var newElement = function() {
+   
+//     var newSkillEl = document.querySelector(".new-skills");
+//     console.log(newSkillEl.textContent);
+//     newSkillEl.textContent = "hello world";
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
