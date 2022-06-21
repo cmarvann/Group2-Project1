52bdd4dc2709821c03a436012c7ee6f071f382b6
Acadia Huber <horsinaround235@gmail.com>
2022-06-21 07:18:33 -0400

    fetch when button is clicked

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -2,3 +2,23 @@ var skill = "";
 var skillClicked = "";
 var skillClose = "";
 var reset = "";
+
+var dndUrl="https://www.dnd5eapi.co/api/skills/";
+
+document.getElementById('nature').addEventListener('click', function(){
+
+    fetch(dndUrl + "nature")
+         .then(response => response.json())
+         .then(json => {
+              console.log(json);
+         });
+});
+
+document.getElementById('acrobatics').addEventListener('click', function(){
+
+    fetch(dndUrl + "acrobatics")
+         .then(response => response.json())
+         .then(json => {
+              console.log(json);
+         });
+});
