9b419268b3d92a8db72d5ef3d353874fa5294e47
crainone4037 <rainonecarmine0@gmail.com>
2022-06-21 20:38:50 -0400

    description/append list

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -6,101 +6,244 @@ var reset = "";
 
 
 
-// //container for skills-list div 
- var addSkillel = document.querySelector("#form");
-
-
-
-
-
-
-
-
-
-
-
-//function will fetch the skills and convert to JSON
-
-
-
-
 
 var dndUrl = "https://www.dnd5eapi.co/api/skills/";
 
+var skillEl = document.querySelector(".add-skills");
 
 
 
+document.getElementById('nature').addEventListener('click', function(){
+
+    fetch(dndUrl + "nature")
+         .then(response => response.json())
+         .then(json => {
+          var addSkillel = document.createElement("li");
+          addSkillel.textContent = (json.desc[0]);
+          skillEl.appendChild(addSkillel);
+         });
+});
+
+document.getElementById('acrobatics').addEventListener('click', function(){
+
+    fetch(dndUrl + "acrobatics")
+         .then(response => response.json())
+         .then(json => {
+          var addSkillel = document.createElement("li");
+          addSkillel.textContent = (json.desc[0]);
+          skillEl.appendChild(addSkillel);
+         });
+});
+
+document.getElementById('animal-handling').addEventListener('click', function(){
+
+     fetch(dndUrl + "animal-handling")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+ 
+
+ document.getElementById('arcana').addEventListener('click', function(){
+
+     fetch(dndUrl + "arcana")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ document.getElementById('athletics').addEventListener('click', function(){
+
+     fetch(dndUrl + "athletics")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+              addSkillel.textContent = (json.desc[0]);
+              skillEl.appendChild(addSkillel);
+          });
+ });
+
+ document.getElementById('deception').addEventListener('click', function(){
+
+     fetch(dndUrl + "deception")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ 
+ document.getElementById('history').addEventListener('click', function(){
+
+     fetch(dndUrl + "history")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ document.getElementById('insight').addEventListener('click', function(){
+
+     fetch(dndUrl + "insight")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ document.getElementById('intimidation').addEventListener('click', function(){
+
+     fetch(dndUrl + "intimidation")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ 
+ document.getElementById('investigation').addEventListener('click', function(){
+
+     fetch(dndUrl + "investigation")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ 
+ document.getElementById('medicine').addEventListener('click', function(){
+
+     fetch(dndUrl + "medicine")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ document.getElementById('perception').addEventListener('click', function(){
+
+     fetch(dndUrl + "perception")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+ 
+ document.getElementById('performance').addEventListener('click', function(){
+
+     fetch(dndUrl + "performance")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+ 
+
+ document.getElementById('persuasion').addEventListener('click', function(){
+
+     fetch(dndUrl + "persuasion")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+ 
+ document.getElementById('religion').addEventListener('click', function(){
+
+     fetch(dndUrl + "religion")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+ 
+ document.getElementById('sleight-of-hand').addEventListener('click', function(){
+
+     fetch(dndUrl + "sleight-of-hand")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+
+ document.getElementById('stealth').addEventListener('click', function(){
+
+     fetch(dndUrl + "stealth")
+          .then(response => response.json())
+          .then(json => {
+               var addSkillel = document.createElement("li");
+               addSkillel.textContent = (json.desc[0]);
+               skillEl.appendChild(addSkillel);
+          });
+ });
+ 
+ document.getElementById('survival').addEventListener('click', function(){
+
+     fetch(dndUrl + "survival")
+          .then(response => response.json())
+          .then(json => {
+               
+          
+              var addSkillel = document.createElement("li");
+              addSkillel.textContent = (json.desc[0]);
+              skillEl.appendChild(addSkillel);
 
 
 
-    fetch(dndUrl).then(function (response) {
+               
+          });
 
-        console.log(dndUrl);
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-
-            })
-        }
-
-        
+          
 
 
-    });
+   });
 
-
-   var newElement = function(){
-
-
-    var singleSkillEl = document.querySelector(".single-skill");
-
-    singleSkillEl.addEventListener("click",function(event){
-
-        if(event){
-            singleSkillEl.appendChild(addSkillel);
-
-        }
-        
-
-
-        
-        
-
-     })
-       
-    
-    } 
-            
-       
-    
-    
-
-
-
-
-    
-
-
-
-
-
-
-
-//  document.querySelector(".single-skill").addEventListener("click",function(event){
-//      console.log("hello world");
-//      console.log(event.target);
-
-     //  })
-
-
-// var newElement = function() {
    
-//     var newSkillEl = document.querySelector(".new-skills");
-//     console.log(newSkillEl.textContent);
-//     newSkillEl.textContent = "hello world";
+ 
+ 
+ 
 
-// }
+ 
 
+ 
+
+
+    
+ 
+
+
+ 
+
+ 
+ 
 
 
 
